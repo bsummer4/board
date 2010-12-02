b299526874027acd871f4fa0528370b49c43429e
Benjamin Summers <bsummer4@eecs.utk.edu>
2010-12-02 16:32:34 -0500

    Rewrite drawing code, and added support for square selection.

--- board.c
+++ board.c
@@ -9,12 +9,13 @@ Mousectl *mousectl;
 const int X=400, Y=400;
 double nx=8, ny=8;
 Point zero={0,0};
-Image *red, *blue;
+Image *red, *blue, *green;
 int numtiles=-1;
 Image **ts=nil;
 Display *d;
+Point selected = {0,0};
 
-void fuck () { fprint(2, "fuck%r"); threadexits("fuck"); }
+void fuck () { fprint(2, "fuck: %r"); threadexits("fuck"); }
 
 int layout[8][8] = {
 	{ 0,  1,  2,  3,  4,  5,  6,  7},
@@ -26,27 +27,20 @@ int layout[8][8] = {
 	{ 0,  0,  0,  0,  0,  0,  0,  0},
 	{11, 11, 11, 11, 11, 11, 11, 11}};
 
-void grid () {
-	double dx=1/nx, dy=1/ny;
-	int color=0;
-	for (double ii=0; ii<1-(dx/2); ii+=dx,++color)
-		for (double jj=0; jj<1-(dy/2); jj+=dy,++color) {
-			color%=2;
-			Point rect[4] = {{ii*X,jj*Y}, {(ii+dx)*X,jj*Y},
-			                 {(ii+dx)*X, (jj+dy)*Y}, {ii*X,(jj+dy)*Y}};
-			fillpoly(screen, rect, 4, 0, color?blue:red, zero); }
-	for (int ii=0; ii<nx; ii++)
-		for (int jj=0; jj<ny; jj++) {
-			int x=(X*ii)/nx, y=(Y*jj)/ny;
-			Rectangle r={(Point){2+x,2+y}, (Point){100+x,100+y}};
-			int piece = layout[jj][ii];
-			if (-1!=piece) draw(screen, r, ts[piece], ts[piece], zero); }
-	for (double ii=0; ii<1+(dx/2); ii+=dx) {
-		Point p1={ii*X,0}, p2={ii*X, Y};
-		line(screen, p1, p2, Enddisc, Enddisc, 1, d->black, zero); }
-	for (double ii=0; ii<1+(dy/2); ii+=dy) {
-		Point p1={0,ii*Y}, p2={X, ii*Y};
-		line(screen, p1, p2, Enddisc, Enddisc, 1, d->black, zero); }}
+void drawsquare (int x, int y) {
+	int color = (x+y)%2;
+	int sel = selected.x==x && selected.y==y;
+#	define XX(i) ((i)*X)/nx
+#	define YY(i) ((i)*Y)/ny
+	Point rect[5] = {
+		{XX(x), YY(y)},         {XX(x+1)+1, YY(y)},
+		{XX(x+1)+1, YY(y+1)+1}, {XX(x), YY(y+1)+1},
+      {XX(x), YY(y)}};
+	fillpoly(screen, rect, 4, 0, color?blue:red, zero);
+	poly(screen, rect, 5, 0, Enddisc, Enddisc, sel?green:d->black, zero);
+	Rectangle r={rect[0], rect[2]};
+	int p = layout[y][x];
+	if (-1!=p) draw(screen, r, ts[p], ts[p], zero); }
 
 void tileset_ (char *buf, int dirlen, int ii) {
 	if (99<ii) goto nomore;
@@ -60,7 +54,7 @@ void tileset_ (char *buf, int dirlen, int ii) {
 	return;
 nomore:
 	if (!ii) errx(1, "No valid images");
-	fprint(2, "hi %d%r", ii);
+	fprint(2, "hi %d\n", ii);
 	numtiles = ii;
 	ts = malloc(ii*sizeof(Image*));
 	if (!ts) err(1, "malloc"); }
@@ -77,14 +71,17 @@ void threadmain (int argc, char *argv[]) {
 		threadexits("initdraw"); }
 	d=display;
 	if (argc != 2)  {
-		fprint(2, "usage: %s %s%r", *argv, "tileset");
+		fprint(2, "usage: %s %s\n", *argv, "tileset");
 		threadexits("invalid arguments"); }
 	tileset (argv[1]);
 	red = allocimage(d, Rect(0, 0, 1, 1), RGB24, 1, DRed);
 	blue = allocimage(d, Rect(0, 0, 1, 1), RGB24, 1, DBlue);
-	red = allocimage(d, Rect(0, 0, 1, 1), RGB24, 1, DRed);
-	blue = allocimage(d, Rect(0, 0, 1, 1), RGB24, 1, DBlue);
+	green = allocimage(d, Rect(0, 0, 1, 1), RGB24, 1, DGreen);
 	mousectl = initmouse(nil, screen);
-	grid();
+	drawsquare(3, 3);
+	for (int ii=0; ii<nx; ii++)
+		for (int jj=0; jj<ny; jj++)
+			drawsquare(ii, jj);
+	drawsquare(selected.x, selected.y);
 	for (;;) { readmouse(mousectl); }
 	threadexits(nil); }
