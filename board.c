566d2610c34a63c1206173f044f22c4f0d4bc377
Benjamin Summers <bsummer4@eecs.utk.edu>
2010-12-14 16:13:55 -0500

    Too many changes for one commit.
    The UI basically works now.  There is a pallet on the right, and mouse
    selection and movement works.

--- board.c
+++ board.c
@@ -7,25 +7,23 @@
 
 Mousectl *mousectl;
 int grid = 50;
-double nx=8, ny=8;
+int nx=11, ny=8, sep=8;
 Point zero={0,0};
-Image *red, *blue, *green;
+Image *bg1, *bg2, *green;
 int numtiles=-1;
 Image **ts=nil;
 Display *d;
 Point selected = {0,0};
 
-void fuck () { fprint(2, "fuck: %r"); threadexits("fuck"); }
-
-int layout[8][8] = {
-	{ 0,  1,  2,  3,  4,  5,  6,  7},
-	{ 8,  9, 10, 11,  0,  1,  2,  3},
-	{-1, -1, -1, -1, -1, -1, -1, -1},
-	{-1, -1, -1, -1, -1, -1, -1, -1},
-	{-1, -1, -1, -1, -1, -1, -1, -1},
-	{-1, -1, -1, -1, -1, -1, -1, -1},
-	{ 0,  0,  0,  0,  0,  0,  0,  0},
-	{11, 11, 11, 11, 11, 11, 11, 11}};
+int layout[8][11] = {
+	{ 0,  1,  2,  3,  4,  5,  6,  7, -1,  0,  6},
+	{ 8,  9, 10, 11,  0,  1,  2,  3, -1,  1,  7},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1,  2,  8},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1,  3,  9},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1,  4, 10},
+	{-1, -1, -1, -1, -1, -1, -1, -1, -1,  5, 11},
+	{ 0,  0,  0,  0,  0,  0,  0,  0, -1, -1, -1},
+	{11, 11, 11, 11, 11, 11, 11, 11, -1, -1, -1}};
 
 void drawsquare (int x, int y) {
 	int color = (x+y)%2;
@@ -36,7 +34,7 @@ void drawsquare (int x, int y) {
 		{XX(x), YY(y)},     {XX(x+1), YY(y)},
 		{XX(x+1), YY(y+1)}, {XX(x), YY(y+1)},
       {XX(x), YY(y)}};
-	fillpoly(screen, rect, 4, 0, color?blue:red, zero);
+	fillpoly(screen, rect, 4, 0, color?bg2:bg1, zero);
 	poly(screen, rect, 5, 0, Enddisc, Enddisc, sel?green:d->black, zero);
 	Rectangle r={rect[0], rect[2]};
 	int p = layout[y][x];
@@ -54,7 +52,6 @@ void tileset_ (char *buf, int dirlen, int ii) {
 	return;
 nomore:
 	if (!ii) errx(1, "No valid images");
-	fprint(2, "hi %d\n", ii);
 	numtiles = ii;
 	ts = malloc(ii*sizeof(Image*));
 	if (!ts) err(1, "malloc"); }
@@ -65,6 +62,14 @@ void tileset (char *dirname) {
 	strcpy(buf, dirname);
 	tileset_(buf, dirlen, 0); }
 
+void DRAW () {
+	for (int ii=0; ii<nx; ii++)
+		for (int jj=0; jj<ny; jj++)
+			if (ii != sep) drawsquare(ii, jj);
+	if (selected.x < nx && selected.y < ny && selected.x != sep)
+		drawsquare(selected.x, selected.y);
+	flushimage(d,1); }
+
 void threadmain (int argc, char *argv[]) {
 	if (-1 == initdraw(nil, nil, "board")) {
 		fprint(2, "board: can't open display: %r");
@@ -74,14 +79,41 @@ void threadmain (int argc, char *argv[]) {
 		fprint(2, "usage: %s %s\n", *argv, "tileset");
 		threadexits("invalid arguments"); }
 	tileset (argv[1]);
-	red = allocimage(d, Rect(0, 0, 1, 1), RGB24, 1, DRed);
-	blue = allocimage(d, Rect(0, 0, 1, 1), RGB24, 1, DBlue);
+	bg1 = allocimage(d, Rect(0, 0, 1, 1), RGB24, 1, DDarkgreen);
+	bg2 = allocimage(d, Rect(0, 0, 1, 1), RGB24, 1, DBluegreen);
 	green = allocimage(d, Rect(0, 0, 1, 1), RGB24, 1, DGreen);
 	mousectl = initmouse(nil, screen);
-	drawsquare(3, 3);
-	for (int ii=0; ii<nx; ii++)
-		for (int jj=0; jj<ny; jj++)
-			drawsquare(ii, jj);
-	drawsquare(selected.x, selected.y);
-	for (;;) { readmouse(mousectl); }
+	DRAW();
+	readmouse(mousectl);
+	for (;;) {
+		Mouse m;
+		int mem;
+		Alt alts[] = {
+			{mousectl->c, &m, CHANRCV},
+			{mousectl->resizec, &mem, CHANRCV},
+			{nil, nil, CHANEND}};
+
+		switch (alt(alts)){
+		case 0: {
+			int mut=0;
+			if (m.buttons&1) {
+				Point tmp = selected;
+				selected = divpt(m.xy,grid);
+				mut=!eqpt(selected, tmp); }
+			if (m.buttons&4) {
+				Point to = divpt(m.xy, grid);
+				if (to.x<sep && to.y<ny &&
+				    selected.x < nx && selected.y < ny &&
+				    !eqpt(to, selected) &&
+				    (selected.x > sep || -1!=layout[selected.y][selected.x])) {
+					mut=1;
+					int p = layout[selected.y][selected.x];
+					if (selected.x < sep) layout[selected.y][selected.x] = -1;
+					if (to.x < sep) layout[to.y][to.x] = p; }}
+			if (mut) DRAW(); }
+			break;
+		case 1:
+			if (0 > getwindow(display, Refnone))
+				err(1, "Couldn't attach to window");
+			DRAW(); }}
 	threadexits(nil); }
